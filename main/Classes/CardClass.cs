dfc3c347a2f1660bce99a63a2c01cc301b11c2dc
PaulFHTW <paul.bogza@gmail.com>
2023-09-24 11:41:55 +0200

    singleton implementation of GameClass

--- main/Classes/CardClass.cs
+++ main/Classes/CardClass.cs
@@ -1,51 +1,77 @@
 using System;
 
 namespace CardClasses{
+    public enum ElementType{
+        Normal, 
+        Fire, 
+        Water
+    };
     abstract public class Card{
-        protected int Damage;
-        protected string Element = "TBD";
-        protected string Type = "TBD";
-        public virtual void Attack(Card Target){
-            System.Console.WriteLine("Attacking");
+        protected string _name= "TBD";
+        public string name{
+            get{return _name;}
+            set{_name = value;}
         }
+        protected int _damage;
+        public int damage{
+            get{return _damage;}
+            set{_damage = value;}
+        }
+        protected ElementType _element;
+        public ElementType element{
+            get{return _element;}
+            set{_element = value;}
+        }
+        protected string _type = "TBD";
+        public string type{
+            get{return _type;}
+            set{_type = value;}
+        }
+        public abstract void Attack(Card Target);
     }
 
     public class MonsterCard : Card{
-        private int HealthPoints;
+        private int _healthPoints;
         public int healthPoints {
-            get{return HealthPoints;}
-            set{HealthPoints = value;}
+            get{return _healthPoints;}
+            set{_healthPoints = value;}
         }
-        private bool IsScared = false;
+        private bool _isScared = false;
         public bool isScared{
-            get{return IsScared;}
-            set{IsScared = value;}
+            get{return _isScared;}
+            set{_isScared = value;}
         }
-        public MonsterCard(int Damage, int HealthPoints, string Element, string Type){
-            this.Damage = Damage;
-            this.HealthPoints = HealthPoints;
-            this.Element = Element;
-            this.Type = Type;
+        public MonsterCard(string name, int damage, int healthPoints, ElementType element, string type){
+            _name = name;
+            _damage = damage;
+            _healthPoints = healthPoints;
+            _element = element;
+            _type = type;
         }
-        public override void Attack(Card Target){}
+        public override void Attack(Card Target){
+            System.Console.WriteLine("{0} attacked {1}", this.name, Target.name);
+        }
     }
 
     public class SpellCard : Card{
-        private bool Effective = true;
+        private bool _effective = true;
         public bool effective{
-            get{return Effective;}
-            set{Effective = value;}
+            get{return _effective;}
+            set{_effective = value;}
         }
-        private bool Effect = true;
+        private bool _effect = true;
         public bool effect{
-            get{return Effect;}
-            set{Effect = value;}
+            get{return _effect;}
+            set{_effect = value;}
         }
-        public SpellCard(int Damage, string Element, string Type){
-            this.Damage = Damage;
-            this.Element = Element;
-            this.Type = Type;
+        public SpellCard(string name, int damage, ElementType element, string type){
+            _name = name;
+            _damage = damage;
+            _element = element;
+            _type = type;
         }
-        public override void Attack(Card Target){}
+        public override void Attack(Card Target){
+            System.Console.WriteLine("{0} used against {1}", this.name, Target.name);
+        }
     }
 }
