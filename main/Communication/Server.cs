76a83cbf8049049e24239f3d25f18451edb4ac56
PaulFHTW <paul.bogza@gmail.com>
2023-10-05 10:37:38 +0200

    server prototype

---
+++ main/Communication/Server.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket serverSock = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Stream, 
+            ProtocolType.Tcp
+        );
+
+        serverSock.Bind(new IPEndPoint(IPAddress.Loopback, 5555));
+        serverSock.Listen(5);
+
+        Socket clientSock = serverSock.Accept();
+        
+        clientSock.Send(Encoding.ASCII.GetBytes("Hello World!"));
+
+        byte[] buffer = {};
+
+        int length = clientSock.Receive(buffer, 0 ,1024, SocketFlags.None);
+        string input = Encoding.ASCII.GetString(buffer, 0, length);
+
+
